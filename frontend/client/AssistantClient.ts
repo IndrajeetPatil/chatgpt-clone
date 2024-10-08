4becaaac33b1c44c378aee64d64932dce149c8e3
Indrajeet Patil <patilindrajeet.science@gmail.com>
2024-10-09 00:53:51 +0200

    frontend logger

--- frontend/client/AssistantClient.ts
+++ frontend/client/AssistantClient.ts
@@ -1,11 +1,17 @@
 import axios from "axios";
 
+import { logger } from "@/utils/logger";
+
+const log = logger.child({ module: "totoro" });
+
 export default async function fetchAssistantResponse(
   url: string,
   model: string,
   temperature: number,
   prompt: string
 ) {
+  log.debug(`Fetching assistant response from ${url}`);
+  log.debug(`Model: ${model}, Temperature: ${temperature}, Prompt: ${prompt}`);
   const response = await axios.post(url, {
     model,
     temperature,
--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,29 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "http://localhost:8000/",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,DELETE,PATCH,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-          { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
-          { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
-        ],
-      },
-    ];
+  reactStrictMode: true,
+  experimental: {
+    serverComponentsExternalPackages: ["pino", "pino-pretty"],
   },
 };
 
