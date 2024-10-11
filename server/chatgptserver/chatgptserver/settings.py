fcd1f7f00523fa2445078bda0848f2edba840335
Indrajeet Patil <patilindrajeet.science@gmail.com>
2024-10-12 00:52:50 +0200

    Update settings.py

--- server/chatgptserver/chatgptserver/settings.py
+++ server/chatgptserver/chatgptserver/settings.py
@@ -45,6 +45,7 @@ REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
+ALLOWED_HOSTS = ["*"]
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
