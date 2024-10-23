c53838835016114af2dd49f9bcecc19c0265ac54
Indrajeet Patil <patilindrajeet.science@gmail.com>
2024-10-23 23:42:42 +0200

    fix eslint config

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -2,6 +2,9 @@ import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
+  {
+    ignores: ["node_modules", ".next"],
+  },
   eslint.configs.recommended,
   ...tseslint.configs.recommended
 );
