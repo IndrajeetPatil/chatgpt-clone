f002c97cb207ac3d1dca227f68a718ac9e6d53ad
Indrajeet Patil <patilindrajeet.science@gmail.com>
2024-10-24 21:45:33 +0200

    bump ruff

--- server/chatgptserver/api/azure_client.py
+++ server/chatgptserver/api/azure_client.py
@@ -1,4 +1,3 @@
-# pylint: disable=too-few-public-methods
 import logging
 import time
 
@@ -11,7 +10,9 @@ logger = logging.getLogger("django")
 
 
 class AzureOpenAIClient:
-    """A singleton client for Azure OpenAI, ensuring thread-safe and efficient usage across multiple requests.
+    """A singleton client for Azure OpenAI
+
+    This ensures thread-safe and efficient usage across multiple requests.
 
     Note:
     The default timeout for Azure OpenAI is 10 minutes, which is sufficient for most use cases.
